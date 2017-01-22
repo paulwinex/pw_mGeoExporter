863aa7d082d88e8f471cf38adf4be4bc10ca8b4f
paulwinex <paulwinex@gmail.com>
2017-01-22 14:42:49 +0400

    fi readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mGeo Exporter
+# mGeo Exporter v1.2
 ##### (Recreated repository)
 
 New repository with new version.
@@ -11,3 +11,13 @@ New repository with new version.
 - Now you can save options presets to file and quick load back to export set
 - Frame padding for cache
 - Compress geo and bgeo to new SC archive (Houdini14)
+
+Full feature list you can found in manuals
+
+### install
+1. Copy folder pw_mGeoExporter to the PYTHONPATH folder
+2. In ScriptEditor execute code:
+```python
+import pw_mGeoExporter
+pw_mGeoExporter.show()
+```
--- __init__.py
+++ __init__.py
@@ -4,21 +4,20 @@ act={name:mGeo exporter,action:show()}
 '''moduleInfo
 Pipline exporter Maya to Houdini
 '''
-# mGeo Exporter v1.0
-# By PaulWinex (paulwinex@gmail.com http://paulwinex.com/) 2013
+# mGeo Exporter v1.2
+# By PaulWinex (paulwinex@gmail.com http://paulwinex.com/) 2013-2017
 # Much thanks for the help:
 #   Somesanctus (somesanctus.blogspot.ru)
 # This product is not licensed
 
 # Using:
-# 1. Copy folder pw_mGeo to the PYTHONPATH folder ( <mayaInstallFolder>\Python\Lib\site-packages )
+# 1. Copy folder pw_mGeoExporter to the PYTHONPATH folder ( <mayaInstallFolder>\Python\Lib\site-packages )
 # 2. In ScriptEditor execute code:
-#import pw_mGeoExporter
-#pw_mGeoExporter.show()
+# import pw_mGeoExporter
+# pw_mGeoExporter.show()
 # import sys, os
-# root = os.path.dirname(__file__)
-# if not root in sys.path:
-#     sys.path.append(root)
+
+
 def show():
     from . import mGeo
     reload(mGeo)
